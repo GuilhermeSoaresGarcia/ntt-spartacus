af5c21eaf3bd5e199274d180fb2c56bd1a089ec4
Guilherme Soares Garcia <guilherme.garcia@email.com>
2024-04-04 18:43:02 -0300

    refactor: alterada a cor do balãozinho de quantidade no mini-cart de azul para verde e removidos alguns trechos de códigos não usados

--- src/app/spartacus/features/custom_components/mini-cart/custom-mini-cart.component.ts
+++ src/app/spartacus/features/custom_components/mini-cart/custom-mini-cart.component.ts
@@ -1,8 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ActiveCartService } from '@spartacus/core';
-import { ICON_TYPE, MiniCartComponent } from '@spartacus/storefront';
-import { Observable } from 'rxjs';
-import { filter, map, startWith } from 'rxjs/operators';
+import { MiniCartComponent } from '@spartacus/storefront';
 
 @Component({
   selector: 'custom-mini-cart',
@@ -11,18 +9,6 @@ import { filter, map, startWith } from 'rxjs/operators';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CustomMiniCartComponent extends MiniCartComponent {
-  iconTypes = ICON_TYPE;
-
-  quantity$: Observable<number> = this.activeCartService.getActive().pipe(
-    startWith({ deliveryItemsQuantity: 0 }),
-    map((cart) => cart.deliveryItemsQuantity || 0)
-  );
-
-  total$: Observable<string> = this.activeCartService.getActive().pipe(
-    filter((cart) => !!cart.totalPrice),
-    map((cart) => cart?.totalPrice?.formattedValue || "")
-  );
-
   constructor(protected activeCartService: ActiveCartService) {
     super(activeCartService)
   }
--- src/app/spartacus/features/custom_components/mini-cart/custom-mini-cart.module.ts
+++ src/app/spartacus/features/custom_components/mini-cart/custom-mini-cart.module.ts
@@ -1,14 +1,10 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import {
-  CmsConfig,
-  I18nModule,
-  provideDefaultConfig,
-  UrlModule,
-} from '@spartacus/core';
-import { CustomMiniCartComponent } from './custom-mini-cart.component';
-import { IconModule } from '@spartacus/storefront';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { UrlModule, I18nModule, provideDefaultConfig, CmsConfig } from "@spartacus/core";
+import { IconModule } from "@spartacus/storefront";
+import { CustomMiniCartComponent } from "./custom-mini-cart.component";
+
 
 @NgModule({
   imports: [CommonModule, RouterModule, UrlModule, IconModule, I18nModule],  
